493ef29220818a1995701dd94227b8ad32f4cb18
Teddy <tedrandby@gmail.com>
2019-12-06 00:51:56 -0500

    trying other ones

--- app.js
+++ app.js
@@ -24,11 +24,9 @@ app.use(cookieParser());
 app.use(cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
